26d6eda3428f75882de431f2e931547dbd8191c2
jpaobandong <jpaobandong@gmail.com>
2016-02-17 18:18:16 +0800

    Added 'MyTester' JUnitTester Class

---
+++ MyTester.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MyTester {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSquare() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCube() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNfactorial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBinarySearch() {
+		fail("Not yet implemented");
+	}
+
+}
